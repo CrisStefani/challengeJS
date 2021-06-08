42f939d671ad946bbc5b1bcab6761855b9768326
CrisStefani <stefani.cristian@yahoo.com.ar>
2021-06-08 16:55:32 -0300

    migration

---
+++ App/migrations/20210608194254-create-operation.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Operations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      concepto: {
+        type: Sequelize.STRING
+      },
+      monto: {
+        type: Sequelize.DECIMAL
+      },
+      fecha: {
+        type: Sequelize.DATE
+      },
+      transaccion: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Operations');
+  }
+};
---
+++ App/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    usuario: DataTypes.STRING,
+    nombre: DataTypes.STRING,
+    contraseña: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};
